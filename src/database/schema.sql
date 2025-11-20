6058b7fddfef8dff2f0a8b7dafa4023b927394f9
Lucas Faria <lucasfaria@outlook.pt>
2025-11-20 10:39:15 +0000

    Update the database schema

--- src/database/schema.sql
+++ src/database/schema.sql
@@ -13,16 +13,8 @@ CREATE TABLE list(
 CREATE TABLE product(
     id INT PRIMARY KEY,
     name TEXT NOT NULL,
-    quantity INT NOT NULL DEFAULT 1,
-    bought INT NOT NULL DEFAULT 0,
+    quantity INT NOT NULL DEFAULT 1 CHECK(quantity > 0),
+    bought INT NOT NULL DEFAULT 0 CHECK(bought >= 0),
     soft_delete BOOLEAN NOT NULL DEFAULT 0,
     list_id REFERENCES list(id)  NOT NULL
-);
-
-CREATE TABLE product_update(
-    id INT PRIMARY KEY,
-    product_id INT REFERENCES product(id) NOT NULL,
-    quantity_diff INT NOT NULL DEFAULT 0,
-    bought_diff INT NOT NULL DEFAULT 0,
-    is_sync BOOLEAN NOT NULL DEFAULT 0
 );
